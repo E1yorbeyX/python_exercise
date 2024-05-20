88ba680cf26385617616f2d96197426862fc3418
Elyor <e1yorbeyx@gmail.com>
2024-05-20 18:42:30 +0500

    finished exercise one

--- exercise_1.py
+++ exercise_1.py
@@ -11,3 +11,21 @@ if x.isdigit() and y.isdigit():
         print(f'The result is: {x} + {y} = {x+y}')
 else:
     raise TypeError("We allow only integers")
+
+
+
+# The version of shown
+
+def multiplication_or_sum(num1, num2):
+    """Given two integer numbers, return their product only if the product is equal to or lower than 1000. Otherwise, return their sum."""
+    
+    product = num1 * num2
+    if product <= 1000:
+        product = product
+    else:
+        product = num1 + num2
+    
+    return f'The result is: {product}'
+
+x = multiplication_or_sum(4,9)
+print(x)
